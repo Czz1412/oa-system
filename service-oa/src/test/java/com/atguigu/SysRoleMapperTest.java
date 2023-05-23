68f2e7c0a55a9da96237f4330a76a4ba631cb0bb
Czz1412 <2360808527@qq.com>
2023-05-23 21:03:28 +0800

    整合MyBatisPlus并进行测试

---
+++ service-oa/src/test/java/com/atguigu/SysRoleMapperTest.java
@@ -0,0 +1,24 @@
+package com.atguigu;
+
+import com.atguigu.auth.mapper.SysRoleMapper;
+import com.atguigu.model.system.SysRole;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class SysRoleMapperTest {
+
+    @Autowired
+    private SysRoleMapper sysRoleMapper;
+
+    @Test
+    public void testSelectById() {
+        List<SysRole> sysRoles = sysRoleMapper.selectList(null);
+        for (SysRole sysRole : sysRoles) {
+            System.out.println(sysRole);
+        }
+    }
+}
